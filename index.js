f91a7f98e1f0dc99f3e419e23f78f1e1d3cc94a7
brandonHafley <bchafley@gmail.com>
2026-06-02 19:46:57 -0500

    add cooldown functionality and property for the Ping command

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
 client.commands = new Collection();
+client.cooldowns = new Collection();
+
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
